7b8d4ef223acc320170b6c5d7d551c251ac47d51
Oxygend <dvf_n@mail.ru>
2016-07-04 10:14:19 +0300

    Code simplification

--- main.go
+++ main.go
@@ -12,26 +12,19 @@ func main() {
 	fmt.Println("Simple file server v1.0")
 	// getting the port from the user
 	fmt.Print("Enter port: ")
-
 	var port string
 	fmt.Scanln(&port)
-
 	// validating the port
-	_, err := strconv.Atoi(port)
-	if err != nil {
+	if _, err := strconv.Atoi(port); err != nil {
 		fmt.Println("Invalid port" + err.Error())
 		os.Exit(1)
 	}
-
 	// getting the executable path
 	wd, _ := osext.ExecutableFolder()
-
 	fmt.Println("Working directory is " + wd)
-
 	// setting up the file server
 	fs := http.FileServer(http.Dir(wd))
 	http.Handle("/", fs)
-
 	// running the file server
 	fmt.Println("Serving the files...")
 	http.ListenAndServe(":" + port, nil)
