955a4816cf898ef061d2ccbcacda55ee98cb4837
Oxygend <dvf_n@Mail.ru>
2016-07-06 19:32:21 +0300

    Specify the settings via application arguments

--- main.go
+++ main.go
@@ -9,22 +9,41 @@ import (
 )
 
 func main() {
-	fmt.Println("Simple file server v1.0")
-	// getting the port from the user
-	fmt.Print("Enter port: ")
-	var port string
-	fmt.Scanln(&port)
-	// validating the port
-	if _, err := strconv.Atoi(port); err != nil {
-		fmt.Println("Invalid port" + err.Error())
-		os.Exit(1)
-	}
-	// getting the executable path
+	// setting the default settings
+	port := "1234"
 	wd, _ := osext.ExecutableFolder()
+	customSettings := false
+
+	// checking for custom settings
+	if len(os.Args) > 1 {
+		customSettings = true
+
+		port = os.Args[1]
+
+		if len(os.Args) > 2 {
+			wd = os.Args[2]
+		}
+	}
+
+	// validating the settings
+	if customSettings {
+		if _, err := strconv.Atoi(port); err != nil {
+			fmt.Println("Invalid port!")
+			os.Exit(1)
+		}
+		if _, err := os.Stat(wd); os.IsNotExist(err) {
+			fmt.Println("Invalid working directory!")
+			os.Exit(2)
+		}
+	}
+
+	fmt.Println("Port is " + port)
 	fmt.Println("Working directory is " + wd)
+
 	// setting up the file server
 	fs := http.FileServer(http.Dir(wd))
 	http.Handle("/", fs)
+
 	// running the file server
 	fmt.Println("Serving the files...")
 	http.ListenAndServe(":" + port, nil)
